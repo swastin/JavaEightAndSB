7b18ab24c365b22910641999d5ff7c351828f43d
swastinuser.email <swastin95.sahoo@gmail.com>
2022-12-21 00:24:00 +0530

    commited

---
+++ javaEight/src/main/java/com/swastin/javaEight/FunctionalInterface/Function/FunctionFi.java
@@ -0,0 +1,10 @@
+package com.swastin.javaEight.FunctionalInterface.Function;
+
+public class FunctionFi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
