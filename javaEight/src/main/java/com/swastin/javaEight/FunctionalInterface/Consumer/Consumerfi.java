4546fefd0136c79f7aca35cde748b4d810157c54
swastinuser.email <swastin95.sahoo@gmail.com>
2022-12-21 13:52:33 +0530

    committed

--- javaEight/src/main/java/com/swastin/javaEight/FunctionalInterface/Consumer/Consumerfi.java
+++ javaEight/src/main/java/com/swastin/javaEight/FunctionalInterface/Consumer/Consumerfi.java
@@ -1,10 +1,24 @@
 package com.swastin.javaEight.FunctionalInterface.Consumer;
 
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.IntConsumer;
+
 public class Consumerfi {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// TODO Auto-generated method stu
+		/*consumer take the input form the user but return nothing*/
+		Consumer<Integer> consume=i->System.out.println("the square of value "+i+"  is  "+i*i);
+		consume.accept(5);
+		/*BiConsumer*/
+		BiConsumer<Integer,Integer> consume1=(i,j)->System.out.print(i*j);
+		consume1.accept(4, 5);
+/**
+ * there is primitive datatype spcific consumer
+ * IntConsumer,LongConsumer and DoubleConsumer
+ * 
+ * */
 	}
 
 }
---
+++ javaEight/src/main/java/com/swastin/javaEight/FunctionalInterface/Supplier/supplierfi.java
@@ -0,0 +1,16 @@
+package com.swastin.javaEight.FunctionalInterface.Supplier;
+
+import java.time.LocalDateTime;
+import java.util.function.Supplier;
+
+public class supplierfi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/*Take no input but provide the out put*/
+		Supplier<LocalDateTime> sup=()->LocalDateTime.now();
+		LocalDateTime localDateTime = sup.get();
+		System.out.println(localDateTime);
+	}
+
+}
