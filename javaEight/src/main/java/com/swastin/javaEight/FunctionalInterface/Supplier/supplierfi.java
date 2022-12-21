63c49c6a9cc38f7ddb075307877700296aa221b8
swastinuser.email <swastin95.sahoo@gmail.com>
2022-12-21 14:00:29 +0530

    commited

--- javaEight/src/main/java/com/swastin/javaEight/FunctionalInterface/Supplier/supplierfi.java
+++ javaEight/src/main/java/com/swastin/javaEight/FunctionalInterface/Supplier/supplierfi.java
@@ -7,7 +7,7 @@ public class supplierfi {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-/*Take no input but provide the out put*/
+		/*Take no input but provide the out put*/
 		Supplier<LocalDateTime> sup=()->LocalDateTime.now();
 		LocalDateTime localDateTime = sup.get();
 		System.out.println(localDateTime);
