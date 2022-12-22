e1024de1cba2fb87452662edbd6cd800e36b9d08
swastinuser.email <swastin95.sahoo@gmail.com>
2022-12-23 00:37:28 +0530

    commited

---
+++ log4jDemo/src/main/java/com/log4j/log4jDemo/App.java
@@ -0,0 +1,13 @@
+package com.log4j.log4jDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
