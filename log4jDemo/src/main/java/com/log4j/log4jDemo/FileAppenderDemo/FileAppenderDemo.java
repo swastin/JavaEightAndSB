26d3e49c9e90c51e1d1cc915e3419917aab2f891
swastinuser.email <swastin95.sahoo@gmail.com>
2022-12-26 18:31:08 +0530

    commited

--- log4jDemo/src/main/java/com/log4j/log4jDemo/FileAppenderDemo/FileAppenderDemo.java
+++ log4jDemo/src/main/java/com/log4j/log4jDemo/FileAppenderDemo/FileAppenderDemo.java
@@ -1,7 +1,8 @@
 package com.log4j.log4jDemo.FileAppenderDemo;
 
 import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.core.Logger;
+//import org.apache.logging.log4j.core.Logger;
+//import org.apache.logging.log4j.
 import org.apache.logging.log4j.*;
 import java.io.*;  
 import java.sql.SQLException;  
@@ -9,11 +10,12 @@ import java.util.*;
 //import org.apache.logging.log4j.Logger;
 
 public class FileAppenderDemo {
-static	org.apache.logging.log4j.Logger log= LogManager.getLogger(FileAppenderDemo.class.getName());
+static	Logger log= LogManager.getLogger(FileAppenderDemo.class.getName());
 			 public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 
 				 log.info("Hello World!!!");
 				 log.warn("Helo world");
+				 log.error("!!!!errorrr!!!");
 	}
 
 }
