f4bd4fb09005d56ee11fd86e2737796c87c6c3b5
swastinuser.email <swastin95.sahoo@gmail.com>
2023-01-02 19:22:47 +0530

    commited

---
+++ SpringSecurityLoginRegisterDemo/src/main/java/com/springsecurity/RgisterloginDemo/SpringSecurityLoginRegisterDemoApplication.java
@@ -0,0 +1,13 @@
+package com.springsecurity.RgisterloginDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityLoginRegisterDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityLoginRegisterDemoApplication.class, args);
+	}
+
+}
---
+++ SpringSecurityLoginRegisterDemo/src/test/java/com/springsecurity/RgisterloginDemo/SpringSecurityLoginRegisterDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsecurity.RgisterloginDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLoginRegisterDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
